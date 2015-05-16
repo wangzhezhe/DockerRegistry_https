f8f021c2fe5d02e957c0f599a37492ccec52eec1
wangzhezhe <W_Hessen@126.com>
2015-05-16 13:33:04 +0800

    Update registry_client.sh

--- Registry_https/registry_client.sh
+++ Registry_https/registry_client.sh
@@ -5,7 +5,8 @@
 #todo: using ssh to control the remote machine and fix the version of images
 echo "input common name:"
 read commonname
-mkdir /etc/docker/certs.d/${commonname}:5000
+mkdir -p /etc/docker/certs.d/${commonname}:5000
+mkdir -p /usr/local/share/ca-certificates
 
 cp ./certs/rootCA.crt /etc/docker/certs.d/${commonname}:5000
 cp ./certs/rootCA.crt /usr/local/share/ca-certificates
